9d59f83412a096763a68ae3469de8bddd6e2267b
valbertoenoc <valbertoenoc@gmail.com>
2026-07-08 22:27:37 -0300

    adpated endpoints to separate business logic

--- main.py
+++ main.py
@@ -1,78 +1,29 @@
-from datetime import datetime, timezone
-
-from fastapi import FastAPI, requests, status
+from fastapi import FastAPI, status
 from fastapi.responses import RedirectResponse, Response
-from pydantic import BaseModel
-from pydantic_settings import BaseSettings
-from starlette.status import HTTP_404_NOT_FOUND
 
-from utils import b62_encode
-
-
-class Settings(BaseSettings):
-    base_url: str = "http://localhost:8000"
-
-
-settings = Settings()
-
+from crud import create_url, get_url
+from deps import SessionDeps
+from models import UrlCreate, UrlPublic
 
 app = FastAPI()
 
-urls: dict[str, dict[str, int | str | datetime]] = {
-    "0": {
-        "id": 0,
-        "short_url": "http://localhost:8000/0",
-        "long_url": "https://www.google.com",
-        "created_at": datetime.now(),
-        "click_count": 0,
-    }
-}
-
-
-class UrlCreate(BaseModel):
-    long_url: str
-
-
-class UrlPublic(BaseModel):
-    id: int
-    short_url: str
-    long_url: str
-    created_at: datetime
-    click_count: int = 0
-
 
 @app.post("/shorten", response_model=UrlPublic)
-async def shorten(url: UrlCreate):
-    id = len(urls)
-    short_code = b62_encode(id)
-    short_url = f"{settings.base_url}/{short_code}"
-
-    new_url = {
-        "id": id,
-        "click_count": 0,
-        "created_at": datetime.now(timezone.utc),
-        "long_url": url.long_url,
-        "short_url": short_code,
-    }
-
-    urls[short_code] = new_url
+async def shorten(session: SessionDeps, url: UrlCreate):
+    new_url = create_url(session=session, url_create=url)
 
     return UrlPublic(
-        id=new_url["id"],
-        click_count=new_url["click_count"],
-        created_at=new_url["created_at"],
-        long_url=new_url["long_url"],
-        short_url=short_url,
+        click_count=new_url.click_count,
+        created_at=new_url.created_at,
+        long_url=new_url.long_url,
+        short_url=new_url.short_url,
     )
 
 
 @app.get("/{short_code}")
-async def redirect(short_code: str):
-    print("short code", short_code)
-    url = urls.get(short_code, "")
+async def redirect(session: SessionDeps, short_url: str):
+    url = get_url(session=session, short_url=short_url)
     if not url:
-        return Response(status_code=HTTP_404_NOT_FOUND)
+        return Response(status_code=status.HTTP_404_NOT_FOUND)
 
-    long_url = url.get("long_url", "")
-    print(long_url)
-    return RedirectResponse(url=long_url, status_code=status.HTTP_302_FOUND)
+    return RedirectResponse(url=url.long_url, status_code=status.HTTP_302_FOUND)
