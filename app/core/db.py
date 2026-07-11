3c74ecb316fcb4ae0bd791397ae033935aa4a7d0
valbertoenoc <valbertoenoc@gmail.com>
2026-07-10 21:16:10 -0300

    removed unused import and reverted async create all commented

--- app/core/db.py
+++ app/core/db.py
@@ -1,8 +1,12 @@
 from sqlalchemy.ext.asyncio import create_async_engine
-from sqlmodel import create_engine
 from sqlmodel.main import SQLModel
 
 from app.core.config import settings
 
 # trying async
 engine = create_async_engine(str(settings.POSTGRES_URL))
+
+# remove this and use alembic instead
+# async def init_db():
+#     async with engine.begin() as conn:
+#         await conn.run_sync(SQLModel.metadata.create_all)
